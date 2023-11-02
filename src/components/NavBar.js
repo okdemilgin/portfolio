8a9dda610fa10480b750d9cf0860c8d78a17a4b3
ilginokdem <okdemilgin@gmail.com>
2023-11-02 20:06:32 +0300

    navbar

--- src/components/NavBar.js
+++ src/components/NavBar.js
@@ -52,7 +52,7 @@ const NavBar = () => {
       </div>
 
       {nav && (
-        <ul className="flex flex-col justify-center items-center absolute top-0 left-0 w-full h-screen bg-gradient-to-b from-black to-gray-800 text-gray-500">
+        <ul className="flex flex-col justify-center items-center absolute top-0 left-0 w-full h-screen bg-gradient-to-b from-black to-gray-800 text-gray-500 z-50">
           {links.map(({ id, link }) => (
             <li key={id} className="px-4 cursor-pointer capitalize py-6 text-4xl">
               <Link onClick={() => setNav(!nav)} to={link} smooth duration={500}>
